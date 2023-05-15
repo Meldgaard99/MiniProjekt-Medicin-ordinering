1951e606b64e2269c686143ed4a68c983e91d0e8
Meldgaard99 <100026274+Meldgaard99@users.noreply.github.com>
2023-05-15 11:04:54 +0200

    adding dagligFastTest2

--- ordination-test/ServiceTest.cs
+++ ordination-test/ServiceTest.cs
@@ -26,34 +26,34 @@ public class ServiceTest
     {
         Assert.IsNotNull(service.GetPatienter());
     }
-/*
-    [TestMethod]
-    public void OpretDagligFast()
-    {
-        Patient patient = service.GetPatienter().First();
-        Laegemiddel lm = service.GetLaegemidler().First();
+    /*
+        [TestMethod]
+        public void OpretDagligFast()
+        {
+            Patient patient = service.GetPatienter().First();
+            Laegemiddel lm = service.GetLaegemidler().First();
 
-        Assert.AreEqual(1, service.GetDagligFaste().Count());
+            Assert.AreEqual(1, service.GetDagligFaste().Count());
 
-        service.OpretDagligFast(patient.PatientId, lm.LaegemiddelId,
-            2, 2, 1, 0, DateTime.Now, DateTime.Now.AddDays(3));
+            service.OpretDagligFast(patient.PatientId, lm.LaegemiddelId,
+                2, 2, 1, 0, DateTime.Now, DateTime.Now.AddDays(3));
 
-        Assert.AreEqual(4, service.GetDagligFaste().Count());
-    }
-    */
+            Assert.AreEqual(4, service.GetDagligFaste().Count());
+        }
+        */
 
 
     [TestMethod]
     public void doegnDosisTest()
     {
-        DagligFast Test1 = new DagligFast( new DateTime(2023, 01, 01), new DateTime(2023, 01, 07), new Laegemiddel("Methotrexat", 0.1, 0.15, 0.16, "Styk"), 2, 2, 1, 0);
+        DagligFast Test1 = new DagligFast(new DateTime(2023, 01, 01), new DateTime(2023, 01, 07), new Laegemiddel("Methotrexat", 0.1, 0.15, 0.16, "Styk"), 2, 2, 1, 0);
 
 
-         double doegnDosisTestCase1 = Test1.doegnDosis();
+        double doegnDosisTestCase1 = Test1.doegnDosis();
 
         Assert.AreEqual(5, doegnDosisTestCase1);
 
-        
+
 
     }
 
@@ -62,32 +62,46 @@ public class ServiceTest
     {
 
         Patient patient = new Patient("121258-0514", "Henrik Hansen", 63.4);
-        
+
         Laegemiddel lm = service.GetLaegemidler().First();
 
         DagligFast Test1 = new DagligFast(new DateTime(2023, 01, 01), new DateTime(2023, 01, 07), new Laegemiddel("Methotrexat", 0.1, 0.15, 0.16, "Styk"), 4, 2, 1, 0);
 
-        double DagligFastTestCase1 = Test1.doegnDosis();
+        double DagligFastTestCase1 = Test1.samletDosis();
 
 
         Assert.AreEqual("Henrik Hansen", patient.navn);
-        Assert.AreEqual(7, DagligFastTestCase1);
-        Assert.AreEqual(6, (Test1.slutDen.Day- Test1.startDen.Day));
+        Assert.AreEqual(49, DagligFastTestCase1);
+        Assert.AreEqual(7, (Test1.slutDen.Day - Test1.startDen.Day + 1));
     }
 
-
-/*
     [TestMethod]
-    [ExpectedException(typeof(ArgumentNullException))]
-    public void TestAtKodenSmiderEnException()
+    public void dagligFastTest2()
     {
-        // Herunder skal man så kalde noget kode,
-        // der smider en exception.
 
-        // Hvis koden _ikke_ smider en exception,
-        // så fejler testen.
-        //Kald noget kode der smider en expetion, eventuelt lav en new med negative værdier
-        Console.WriteLine("Her kommer der ikke en exception. Testen fejler.");
+        Patient patient = service.GetPatienter().FirstOrDefault(p => p.PatientId == 1)!;
+
+        DagligFast Test2 = new DagligFast(new DateTime(2023, 05, 20), new DateTime(2023, 09, 12), new Laegemiddel("Pencilin", 0.1, 0.15, 0.16, "Styk"), 4, 2, 1, 0);
+
+       //  double DagligFastTestCase2 = Test2.doegnDosis();
+
+
+
+        Assert.AreEqual(1, patient.PatientId);
+
     }
-    */
-} 
+    /*
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestAtKodenSmiderEnException()
+        {
+            // Herunder skal man så kalde noget kode,
+            // der smider en exception.
+
+            // Hvis koden _ikke_ smider en exception,
+            // så fejler testen.
+            //Kald noget kode der smider en expetion, eventuelt lav en new med negative værdier
+            Console.WriteLine("Her kommer der ikke en exception. Testen fejler.");
+        }
+        */
+}
